1ff7126fc694892abdaa37795a151a7d35f519e1
smozhaev <kmozhaev100@gmail.com>
2024-11-14 00:50:07 +0300

    Edit fonts, remove extra header

--- frontend_fungi/src/pages/AboutUs/AboutUs.tsx
+++ frontend_fungi/src/pages/AboutUs/AboutUs.tsx
@@ -3,40 +3,32 @@ import './index.css';
 
 const AboutUs: React.FC = () => {
   return (
-    <div className="about">
-      <header className="header">
-        <img src="/logo.png" alt="Fungi logo" className="logo" />
-        <nav>
-          <ul className="nav-links">
-            <li>О нас</li>
-            <li>Публикации</li>
-            <li>Энциклопедия</li>
-          </ul>
-        </nav>
-      </header>
-      
+    <div className="about">      
       <section className="intro">
-        <h1>Fungi - твой помощник в мире грибов.</h1>
-        <p>
-          Команда разработчиков случайно пришла к идее создания простого и эффективного сервиса, помогающего
-          любителям собирать грибы, не напрягаясь.
-        </p>
-        <p>
-          Из этой идеи родилось приложение Fungi – электронная энциклопедия о грибах, которая предоставляет 
-          пользователям информацию о залежах грибов, их приготовлениях, интересных фактах и многом другом.
-        </p>
-        <p><strong>Цель проекта - облегчить жизнь любителям грибов</strong></p>
+        <div className="intro-text-container">
+          <h1>Fungi - твой помощник в мире грибов.</h1>
+          <p className="intro-p">
+            Команда разработчиков случайно пришла к идее создания простого и эффективного сервиса, помогающего
+            любителям собирать грибы, не напрягаясь.
+          </p>
+          <p className="intro-p">
+            Из этой идеи родилось приложение Fungi – электронная энциклопедия о грибах, которая предоставляет
+            пользователям информацию о залежах грибов, их приготовлениях, интересных фактах и многом другом.
+          </p>
+          <p className="intro-p-strong"><strong>Цель проекта - облегчить жизнь любителям грибов</strong></p>
+        </div>
+
       </section>
-      
+
       <section className="features">
         <h2>Всё о грибах в одном месте!</h2>
         <div className="feature-cards">
           <div className="card">
-            <img src="/icon1.png" alt="icon1" />
+            <img src="/icon1.png" alt="icon1"/>
             <p>Офлайн доступ к 100 видам грибов в твоём телефоне.</p>
           </div>
           <div className="card">
-            <img src="/icon2.png" alt="icon2" />
+            <img src="/icon2.png" alt="icon2"/>
             <p>С полезной информацией, чтобы точно отличить мухомор от съедобного гриба.</p>
           </div>
           <div className="card">
@@ -47,7 +39,7 @@ const AboutUs: React.FC = () => {
       </section>
       
       <section className="join">
-        <h3>Стань частью грибного сообщества!</h3>
+        <h2>Стань частью грибного сообщества!</h2>
       </section>
     </div>
   );
